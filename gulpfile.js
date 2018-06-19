56a878ad9a7c202bbb372ecf3d27ca48bd3ee865
Ryan Holstein <Ryan.c.Holstein@biola.edu>
2018-06-18 17:27:49 -0700

    initial

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
+
+gulp.task('connect', function() {
+  connect.server({
+    root: '.',
+    livereload: true
+  })
+});
+
+gulp.task('html', function() {
+  gulp.src(['./**/*.html'])
+  .pipe(connect.reload())
+});
+
+
+// css
+gulp.task('sass', function() {
+  gulp.src('./styling/scss/*.scss')
+  .pipe(sass({style: 'expanded'}))
+    .on('error', gutil.log)
+  .pipe(gulp.dest('./styling/css'))
+  .pipe(connect.reload())
+
+});
+
+//js
+gulp.task('scripts', function() {
+  return gulp.src('./js/*.js')
+    .pipe(concat('all.js'))
+    //.pipe(uglify('all.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+// watch these
+gulp.task('watch', function() {
+  gulp.watch('./build/**/*.html', ['html']);
+  gulp.watch('./styling/scss/*.scss', ['sass']);
+  gulp.watch('./js/*.js', ['scripts']);
+
+});
+
+
+gulp.task('default', ['html', 'sass', 'scripts', 'watch', 'connect']);
