9e67417a72757014baabe078c7e9ff6b83b2cf0e
Ryan Holstein <Ryan.c.Holstein@biola.edu>
2018-06-25 19:36:07 -0700

    canges

--- styling/js/index.js
+++ styling/js/index.js
@@ -1,3 +1,16 @@
+//phonecall Order 
+// 1. voicemail 
+// 2. news 
+// 3. order merch 
+// 4. message 
+// 5. pobox 
+// 6. joke 
+// 7. magic trick 
+// 8. secret 
+// 9. mix 
+// 0. home 
+
+
 var phone_beep = new Audio('./assets/sounds/beep_Sound.mp3')
 
 
@@ -11,33 +24,52 @@ var call_options = {
         audio: new Audio('./assets/sounds/voicemail/sbphone.mp3')
     },
     1 : {
-        audio: new Audio('./assets/sounds/lebron_james_kid.mp3')
+        audio: new Audio('./assets/sounds/voicemail/voicemail.mp3')
     },
     2 : {
-        audio: new Audio('./assets/sounds/miku_world_is_mine.mp3') 
+        audio: new Audio('./assets/sounds/voicemail/news.mp3') 
     },
     3 : {
-        audio: new Audio('./assets/sounds/naruto_talking.mp3') 
-    }
+        audio: new Audio('./assets/sounds/voicemail/order_mercy.m4a') 
+    },
+    4 : {
+        audio: new Audio('./assets/sounds/voicemail/kerwinraymessage.mp3') 
+    },
+
+    5 : {
+        audio: new Audio('./assets/sounds/voicemail/pobox.mp3') 
+    },
+
+    6 : {
+        audio: new Audio('./assets/sounds/voicemail/joke.mp3')
+    },  
+    7 : {
+        audio: new Audio('./assets/sounds/voicemail/sbmagictrick.mp3')
+    },
+    8 : {
+        audio: new Audio('./assets/sounds/voicemail/secrets.mp3')
+    },
+    9 : {
+        audio: new Audio('./assets/sounds/voicemail/mix.mp3')
+    },
+
 }
 
 function buttonPress(num) {
     recentPressed = String(num);
     phoneDial();
-    if (can_select) {
-        var sbSound = call_options[num];
+    stopAllAudio();
+   
+    var sbSound = call_options[num];
+    setTimeout(function(){
         sbSound.audio.play()
+        console.log('timeout Completed')
+    }, 1000)
+    
         
-    }
-
-
     
 }
-function clearNum() {
-    recentPressed = ''
-    phoneDial();
 
-}
 
 
 
@@ -52,12 +84,22 @@ function playMenu() {
     media_menu.currentTime = 0;
     media_menu.play()
 }
-function selectChange() {
-    can_select = !can_select;
-    console.log(can_select);
+// function selectChange() {
+//     can_select = !can_select;
+//     console.log(can_select);
+// }
+
+
+function stopAllAudio() {
+    Object.keys(call_options).forEach(function(item) {
+        call_options[item].audio.pause()
+        call_options[item].audio.currentTime = 0;
+    })
 }
 
+
 window.onload = function() {
-    call_options[0].play();
-    window.setTimeout(selectChange, 2500)
+    var sbmenu = call_options[0]
+    sbmenu.audio.play();
+    //window.setTimeout(selectChange, 2500)
   };
